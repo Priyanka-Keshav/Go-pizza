1665d4841551f25157947cfaa91c9dd95e0f559e
Priyanka-Keshav <priyanka1312200342@gmail.com>
2025-02-12 20:50:12 +0530

    Update GoPizza.js

--- Backend/Config/GoPizza.js
+++ Backend/Config/GoPizza.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/GOPIZZA", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
@@ -11,4 +12,5 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+
 module.exports = connectDB;
